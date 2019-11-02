640a23a13e613f0f1082c1a526d880f7c425bf26
lyhd <lyhong.rupp@gmail.com>
2019-11-02 12:00:45 +1100

    created a sample course id 1

--- routes/courses.js
+++ routes/courses.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.send("Hi, we are now at courses route");
 });
 
+router.get("/1", (req, res) => {
+  res.send("I am now in course #1");
+});
+
 module.exports = router;
